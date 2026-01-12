dde3cfbd16e981302e9a40b61c30ba54eb45facd
Mawasto <mitana.kacper1@gmail.com>
2026-01-12 20:05:54 +0100

    added styles folder with styles for all pages, implemented styles on mainpage, modified navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,18 +1,21 @@
 import { Link } from 'react-router-dom'
-import './Navbar.css'
+import './styles/Navbar.css'
 
 const Navbar = () => (
     <nav className="navbar">
-        <ul>
-            <li><Link to="/">Main Page</Link></li>
-            <li><Link to="/season-results">Results from Season</Link></li>
-            <li><Link to="/driver-stats">Driver Stats</Link></li>
-            <li><Link to="/constructor-stats">Constructor Stats</Link></li>
-            <li><Link to="/track-stats">Track Stats</Link></li>
-            <li><Link to="/pitstops">Pitstops</Link></li>
-            <li><Link to="/compare-drivers">Compare Drivers</Link></li>
-            <li><Link to="/season-calendar">Season Calendar</Link></li>
-        </ul>
+        <div className="nav-inner">
+            <div className="nav-brand">f1HistoryViewer</div>
+            <ul className="nav-links">
+                <li><Link to="/">Main Page</Link></li>
+                <li><Link to="/season-results">Results from Season</Link></li>
+                <li><Link to="/driver-stats">Driver Stats</Link></li>
+                <li><Link to="/constructor-stats">Constructor Stats</Link></li>
+                <li><Link to="/track-stats">Track Stats</Link></li>
+                <li><Link to="/pitstops">Pitstops</Link></li>
+                <li><Link to="/compare-drivers">Compare Drivers</Link></li>
+                <li><Link to="/season-calendar">Season Calendar</Link></li>
+            </ul>
+        </div>
     </nav>
 )
 
